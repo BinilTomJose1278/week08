9bd72dd55ea2ce54b0626337ef289e20a1ce13b0
BinilTomJose1278 <biniltomjose12780@gmail.com>
2025-09-16 06:08:49 +1000

    Code Refactored

--- frontend/main.js
+++ frontend/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //const PRODUCT_API_BASE_URL = 'http://product-service-w04e2:8000';
     // Order Service is named 'order-service-w04e2' and exposes port 8001 internally.
     //const ORDER_API_BASE_URL = 'http://order-service-w04e2:8001';
-
+    //working
     // DOM Elements
     const messageBox = document.getElementById('message-box');
     const productForm = document.getElementById('product-form');
